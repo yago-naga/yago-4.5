f63762a07e5de6836b5ecb78b80ad422af01657e
Paola Ortega Saborio <paola@saborio.org>
2022-04-12 16:59:23 +0200

    Fixed some bugs, put in main clause

--- test.py
+++ test.py
@@ -1,72 +1,76 @@
 import sys
-from wasabi import msg, table
+from wasabi import msg
 import csv
 
-msg.info("Initiated...")
-gold_standard = ""
-output_file = ""
-not_in_output = []
-not_in_gold = []
-
-try:
-    gold_standard = sys.argv[1]
-    output_file = sys.argv[2]
-    msg.text(f"gold standard: {str(gold_standard)}")
-    msg.text(f"output file: {str(output_file)}")
-except:
-    msg.fail("Problem with arguments provided. Please provide the path to the gold standard file and the output file to test.")
-    quit()
-
-with msg.loading("Analyzing the files..."):
-    err_file = "gold standard"
+if __name__ == '__main__':
+    msg.info("Initiated...")
+    gold_standard = ""
+    output_file = ""
+    not_in_output = []
+    not_in_gold = []
+    halt = False
+    
     try:
-        with open(gold_standard) as file:
-            tsv_file = csv.reader(file, delimiter="\t")
-            for golden_line in tsv_file:
-                found = False
-                err_file = "output"
-                if golden_line != []:
-                    with open(output_file) as file2:
-                        tsv_file2 = csv.reader(file, delimiter="\t")
-                        for output_line in tsv_file2:
-                            if golden_line == output_line:
-                                found = True
-                                break
-                        file2.close()
-                    if not found:
-                        not_in_output.append(golden_line)
-        file.close()
+        gold_standard = sys.argv[1]
+        output_file = sys.argv[2]
+        msg.text(f"gold standard: {str(gold_standard)}")
+        msg.text(f"output file: {str(output_file)}")
+    except:
+        msg.fail("Problem with arguments provided. Please provide the path to the gold standard file and the output file to test.")
+        halt = True
 
-        with open(output_file) as file:
-            tsv_file = csv.reader(file, delimiter="\t")
-            for output_line in tsv_file:
-                found = False
-                err_file = "output"
-                if output_line != []:
-                    with open(gold_standard) as file2:
-                        tsv_file2 = csv.reader(file, delimiter="\t")
-                        for golden_line in tsv_file2:
-                            if output_line == golden_line:
-                                found = True
-                                break
-                        file2.close()
-                    if not found:
-                        not_in_gold.append(output_line)
-        file.close()                
-    except FileNotFoundError:
-        msg.fail(f"Invalid file path for {err_file} file.")
+    if halt:
+        exit()
+
+    with msg.loading("Analyzing the files..."):
+        err_file = "gold standard"
         try:
-            exit()
-        except:
-            print("Execution finished")
+            with open(gold_standard) as file:
+                tsv_file = csv.reader(file, delimiter="\t")
+                for golden_line in tsv_file:
+                    found = False
+                    err_file = "output"
+                    if golden_line != []:
+                        with open(output_file) as file2:
+                            tsv_file2 = csv.reader(file2, delimiter="\t")
+                            for output_line in tsv_file2:
+                                if golden_line == output_line:
+                                    found = True
+                                    break
+                            file2.close()
+                        if not found:
+                            not_in_output.append(golden_line)
+            file.close()
 
-msg.good("Files analyzed successfully!")
-msg.text(f"Number of files in golden standard and not in output file: {len(not_in_output)}")
-msg.text("Lines:", color="#FFD700")
-for line in not_in_output:
-    msg.text(f"\t\t{str(line)}")
-msg.text(f"Number of files in output file and not in golden standard: {len(not_in_output)}")
-msg.text("Lines:", color="#FFD700")
-for line in not_in_gold:
-    msg.text(f"\t\t{str(line)}")
+            with open(output_file) as file:
+                tsv_file = csv.reader(file, delimiter="\t")
+                for output_line in tsv_file:
+                    found = False
+                    err_file = "output"
+                    if output_line != []:
+                        with open(gold_standard) as file2:
+                            tsv_file2 = csv.reader(file2, delimiter="\t")
+                            for golden_line in tsv_file2:
+                                if output_line == golden_line:
+                                    found = True
+                                    break
+                            file2.close()
+                        if not found:
+                            not_in_gold.append(output_line)
+            file.close()                
+        except FileNotFoundError:
+            msg.fail(f"Invalid file path for {err_file} file.")
+            halt = True
 
+    if halt:
+        exit()
+
+    msg.good("Files analyzed successfully!")
+    msg.text(f"Number of files in golden standard and not in output file: {len(not_in_output)}")
+    msg.text("Lines:", color="yellow")
+    for line in not_in_output:
+        msg.text(f"\t{str(line)}")
+    msg.text(f"Number of files in output file and not in golden standard: {len(not_in_output)}")
+    msg.text("Lines:", color="yellow")
+    for line in not_in_gold:
+        msg.text(f"\t{str(line)}")
