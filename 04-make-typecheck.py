e5a07c9fd2495bdcf3e9faededf434ea7972be1f
Fabian Suchanek <github@fabian.suchanek.name>
2023-05-01 15:47:03 +0200

    test=false

--- 04-make-typecheck.py
+++ 04-make-typecheck.py
@@ -24,7 +24,7 @@ Algorithm:
    
 """
 
-TEST=True
+TEST=False
 FOLDER="test-data/04-make-typecheck/" if TEST else "yago-data/"
 
 ##########################################################################
