e184256ff2e8c9ad1503150b5d2f16cbdc1a62a3
Fabian Suchanek <github@fabian.suchanek.name>
2023-05-09 01:46:25 +0200

    Exporting YAGO-small

--- 07-export.sh
+++ 07-export.sh
@@ -1,12 +1,17 @@
-echo "Packing YAGO files..."
+echo "Creating small YAGO..."
 cd yago-data
+cp 01-yago-final-schema.ttl yago-small.ttl
+grep -v -P '@prefix' 05-yago-final-taxonomy.tsv >> yago-small.ttl
+grep -P 'yago:A[^\t]+\t[^\t]+\t("|yago:A|schema:)' >> yago-small.ttl
+echo "Done"
+echo "Packing YAGO files..."
 rm yago-4.5.0.zip
 mv 01-yago-final-schema.ttl yago-schema.ttl
 mv 05-yago-final-wikipedia.tsv yago-facts.ttl
 mv 05-yago-final-beyond-wikipedia.tsv yago-beyond-wikipedia.ttl
 mv 05-yago-final-meta.tsv yago-meta-facts.ntx
 mv 05-yago-final-taxonomy.tsv yago-taxonomy.ttl
-zip yago-4.5.0.zip yago-schema.ttl yago-facts.ttl yago-beyond-wikipedia.ttl yago-meta-facts.ntx yago-taxonomy.ttl
+zip yago-4.5.0.zip yago-schema.ttl yago-facts.ttl yago-beyond-wikipedia.ttl yago-meta-facts.ntx yago-taxonomy.ttl yago-small.ttl
 mv yago-schema.ttl 01-yago-final-schema.ttl
 mv yago-facts.ttl 05-yago-final-wikipedia.tsv
 mv yago-beyond-wikipedia.ttl 05-yago-final-beyond-wikipedia.tsv
