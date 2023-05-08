c0a4d2d2bfed1cab43a1c40a780dc0607d0f738d
Fabian Suchanek <github@fabian.suchanek.name>
2023-05-08 07:09:22 +0200

    adding more YAGO 4 stats

--- yago-4-statistics.py
+++ yago-4-statistics.py
@@ -16,7 +16,7 @@ import Prefixes
 from collections import defaultdict
 
 ###########################################################################
-#           Removing shortcuts
+#           Counting and removing shortcuts
 ###########################################################################
 
 yagoTaxonomyUp=defaultdict(set)
@@ -39,13 +39,28 @@ def removeShortcuts():
                 for ss in yagoTaxonomyUp.get(s,[]):
                     removeShortcutParentsOf(c, ss)
  
+###########################################################################
+#           Retrieving superclasses
+###########################################################################
+
+ def getSuperClasses(cls, classes, yagoTaxonomyUp, pathsToRoot):
+    """Adds all superclasses of a class <cls> (including <cls>) to the set <classes>"""
+    classes.add(cls)
+    # Make a check before because it's a defaultdict,
+    # which would create cls if it's not there
+    if cls==Prefixes.schemaThing:
+        pathsToRoot[0]+=1
+    if cls in yagoTaxonomyUp:
+        for sc in yagoTaxonomyUp[cls]:
+            getSuperClasses(sc, classes, yagoTaxonomyUp, pathsToRoot)
+
 ##########################################################################
 #             Main
 ##########################################################################
 
 with TsvUtils.Timer("Collecting YAGO 4 statistics"):
 
-    for triple in TsvUtils.tsvTuples("../yago-4/classes.nt", "  Loading YAGO taxonomy"):
+    for triple in TsvUtils.tsvTuples("../yago-4/classes.nt", "  Loading YAGO 4 taxonomy"):
         if len(triple)>3 and triple[1]=="<http://www.w3.org/2000/01/rdf-schema#subClassOf>":
             yagoTaxonomyUp[triple[0]].add(triple[2])
     
@@ -55,3 +70,28 @@ with TsvUtils.Timer("Collecting YAGO 4 statistics"):
     after=sum(len(yagoTaxonomyUp[s]) for s in yagoTaxonomyUp)
     print("  Taxonomic links after shortcut removal:", after)
     print("  Links removed:", before-after)
+    
+    directClasses=set()
+    currentSubject=""
+    totalEntities=0
+    totalClassesPerInstance=0
+    totalPathsToRoot=0
+    for triple in TsvUtils.tsvTuples("../yago-4/full-types-sorted.nt", "  Running through YAGO 4 types"):
+        if triple[1]!="rdf:type":
+            continue
+        if triple[0]==currentSubject:
+            directClasses.add(triple[2])
+            continue
+        totalEntities+=1
+        superClasses=set()
+        for c in classes:
+            getSuperClasses(c, superClasses, yagoTaxonomyUp, pathsToRoot)
+        totalClassesPerInstance+=len(superClasses)   
+        totalPathsToRoot+=pathsToRoot[0]  
+        directClasses=set()
+        currentSubject=triple[0]
+        directClasses.add(triple[2])
+        
+     print("  Total entities:", totalEntities)
+     print("  Avg paths to root:", totalPathsToRoot/totalEntities)
+     print("  Avg classes per instance:", totalClassesPerInstance/totalEntities)
