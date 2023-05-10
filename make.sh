b1cc1cac906780e87b882c1e065d7a3d8704932c
Fabian Suchanek <github@fabian.suchanek.name>
2023-05-10 07:52:22 +0200

    make

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
-#python3 01-make-schema.py
-#python3 02-make-taxonomy.py
-#python3 03-make-facts.py
+python3 01-make-schema.py
+python3 02-make-taxonomy.py
+python3 03-make-facts.py
 python3 04-make-typecheck.py
 python3 05-make-ids.py
-#python3 06-make-statistics.py
+python3 06-make-statistics.py
