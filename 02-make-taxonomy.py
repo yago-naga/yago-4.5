0f500e89317af18b79fe137f06abf94cffef77d0
Fabian M. Suchanek <suchanek@telecom-paris.fr>
2026-02-21 11:09:50 +0100

    giving preference to awards

--- 02-make-taxonomy.py
+++ 02-make-taxonomy.py
@@ -170,7 +170,6 @@ def addSubClass(superClass, subClass, yagoSchema, yagoTaxonomyUp, wikidataTaxono
     if subClass in yagoSchema.wikidataClasses:
         logWriter.writeMetaFact(subClass, Prefixes.rdfsSubclassOf, superClass, Prefixes.ysReason, '"is mapped to YAGO class"')
         return
-    
         
     # Treat classes that appear already in the taxonomy
     if subClass in yagoTaxonomyUp:
