8cba073ef4437a7bedcf17f9a42ab997ef7be344
Fabian M. Suchanek <suchanek@telecom-paris.fr>
2026-03-31 20:03:53 +0200

    removed mincount check (as it would have to check for properties that the entity does not have in the data)

--- 03-make-facts.py
+++ 03-make-facts.py
@@ -494,16 +494,6 @@ def handleMaxCounts(entityFacts: Graph, yagoSchema: YagoSchema, writer, isSecond
                    else:
                         languages.add(lang)
 
-def checkMinCounts(entityFacts: Graph, yagoSchema: YagoSchema, isSecondaryClass: bool, writer) -> bool:
-    """ TRUE if the object passes the MinCount checks"""
-    mainEntity: str = entityFacts.mainSubject()            
-    for predicate in entityFacts.predicatesOf(mainEntity):        
-        yagoProperty = yagoSchema.properties.get(predicate, None)
-        if yagoProperty and yagoProperty.minCount and len(entityFacts.objectsOf(mainEntity, predicate)) < yagoProperty.minCount and not (isSecondaryClass and predicate == Prefixes.rdfsLabel):
-            self.writer.writeMetaFact(mainEntity, predicate, Prefixes.schemaThing, Prefixes.ysReason, '"mincount failed: just {len(entityFacts.objectsOf(mainEntity, predicate))} objects instead of {yagoProperty.minCount}"')
-            return False
-    return True
-
 def guessLabelIfNecessary(entityFacts: Graph) -> bool:
     """ Tries to guess a label for an entity from a Wikipedia URL"""
     mainEntity: str = entityFacts.mainSubject()            
@@ -579,13 +569,12 @@ class treatWikidataEntity():
         
         handleMaxCounts(entityFacts, self.yagoSchema, self.writer, isSecondaryClass)
 
+        guessLabelIfNecessary(entityFacts)
+        
         if not isSecondaryClass and not guessLabelIfNecessary(entityFacts):
             self.writer.writeMetaFact(entityFacts.mainSubject(), Prefixes.rdfType, Prefixes.schemaThing, Prefixes.ysReason, '"no label"')
             return True
-        
-        if not checkMinCounts(entityFacts, self.yagoSchema, isSecondaryClass, self.writer):
-            return True               
-
+            
         subject=entityFacts.mainSubject()
         for predicate in entityFacts.predicatesOf(subject):
             for obj in entityFacts.objectsOf(subject, predicate):
