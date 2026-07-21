0d2c083df4964a3a8ced554f28d6ee6a9570ff9a
Thomas Bonald <thomas.bonald@telecom-paristech.fr>
2026-07-21 14:23:37 +0200

    coin identifier

--- 02-make-taxonomy.py
+++ 02-make-taxonomy.py
@@ -56,6 +56,7 @@ instanceIndicators = {
     "wdt:P171",  # parent taxon -> instance of taxon
     "wdt:P176",  # manufacturer -> instance of Product
     "wdt:P178",  # developer -> instance of Product
+    "wdt:P10205", # coin identifier -> instance of Product
     "wdt:P11143", # disease identifier
     "wdt:P11430", # disease identifier
     "wdt:P13987", # disease identifier
